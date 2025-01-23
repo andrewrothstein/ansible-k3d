9a34dd8e3b3f94eae4c37c80ef5dea8cefa92b39
Andrew Rothstein <andrew.rothstein@gmail.com>
2025-01-23 18:23:57 +0000

    k3d:5.8.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,39 +1,44 @@
 #!/usr/bin/env sh
 set -e
 DIR=~/Downloads
-MIRROR=https://github.com/rancher/k3d/releases/download
+MIRROR=https://github.com/k3d-io/k3d/releases/download
 APP=k3d
 
 
 dl()
 {
-    local ver=$1
-    local os=$2
-    local arch=$3
-    local dotexe=${4:-}
-    local platform="$os-$arch"
-    local url=$MIRROR/$ver/${APP}-${platform}${dotexe}
-    local lfile=$DIR/${APP}-${ver}-${platform}${dotexe}
-
-    if [ ! -e $lfile ];
-    then
-        curl -sSLf -o $lfile $url
-    fi
-
+    local lchecksums=$1
+    local ver=$2
+    local os=$3
+    local arch=$4
+    local dotexe=${5:-}
+    local platform="${os}-${arch}"
+    local url="${MIRROR}/${ver}/${APP}-${platform}${dotexe}"
+    local file="${APP}-${platform}${dotexe}"
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+    printf "    %s: sha256:%s\n" $platform $(grep -e "$file\$" $lchecksums| awk '{print $1}')
 }
 
 dlver () {
     local ver=$1
+    local lchecksums="${DIR}/${APP}-${ver}-checksums.txt"
+
+    # https://github.com/k3d-io/k3d/releases/download/v5.8.1/checksums.txt
+    local rchecksums="${MIRROR}/${ver}/checksums.txt"
+    if [ ! -e "${lchecksums}" ];
+    then
+        curl -sSLf -o "${lchecksums}" "${rchecksums}"
+    fi
+
+    printf "  # %s\n" $rchecksums
     printf "  %s:\n" $ver
-    dl $ver darwin amd64
-    dl $ver darwin arm64
-    dl $ver linux amd64
-    dl $ver linux 386
-    dl $ver linux arm
-    dl $ver linux arm64
-    dl $ver windows amd64 .exe
+    dl $lchecksums $ver darwin amd64
+    dl $lchecksums $ver darwin arm64
+    dl $lchecksums $ver linux amd64
+    dl $lchecksums $ver linux 386
+    dl $lchecksums $ver linux arm
+    dl $lchecksums $ver linux arm64
+    dl $lchecksums $ver windows amd64 .exe
 }
 
-dlver ${1:-v5.7.5}
+dlver ${1:-v5.8.1}
